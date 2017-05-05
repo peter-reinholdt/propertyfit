abbbd946d5f4f614152371aafce5425678f7c088
Erik Rosendahl Kjellgren <kjellgren@fe1.deic.sdu.dk>
2017-05-05 12:26:50 +0200

    Constraints and q0

--- cost.py
+++ cost.py
@@ -1,10 +1,63 @@
+import chargefit
+import numpy as np
+import scipy.optimize
+
 structures = chargefit.loadfchks("test")
 [s.compute_grid() for s in structures]
 [s.compute_rinvmat() for s in structures]
 [s.compute_qm_esp() for s in structures]
 
-q0 = np.zeros(s[0].natoms)
+def get_q0():
+    # #####################################################
+    #
+    # Gets the start guess of the charges, fourth coloum
+    # in the constraints files 
+    #
+    # #####################################################
+    return constraints[:,3]
+
 def cost(q):
-    return np.average([esp_sum_squared_error(s.rinvmat, s.esp_grid_qm, q) for s in structures])
+    # #####################################################
+    #
+    # Put hard constraints on the total charge, and atoms
+    # that is chosen to have the same charge. The constraints
+    # is forced in such away that nummerical problems can arise
+    # cost function might be discontineous?
+    # Atoms to be constrained to the same charge, can be found
+    # in constraints file;
+    # Constraint file = [Atom, index, same charge index, start charge guess]
+    #
+    # q    = input charge
+    # qout = charge used in RMSD calculation
+    # capq = Change in cap atoms to get total capping charge to zero
+    #
+    # #####################################################
+    
+    # Make same atoms same charge
+    qout=q
+    for i in range(0, len(constraints)):
+        qout[i] = qout[int(constraints[i,2])-1]
 
+    # Assign constraints
+    # Total charge on caps is set to zero
+    capq = qout[0:6].sum()+qout[-6:].sum()
+    qout[0:6] -= (capq)/12
+    qout[-6:] -= (capq)/12
+    
+    # Total molecular charge set to qtot
+    atoms = len(qout)
+    qout[6:atoms-6] -= (qout[6:atoms-6].sum()-qtot)/(atoms-12)
 
+    return np.average([chargefit.esp_sum_squared_error(s.rinvmat, s.esp_grid_qm, qout) for s in structures])
+
+# AA = amino acid, give name as string // make it as input
+AA = 'GLY'
+# qtot = total charge of the amino acid, give number // make it as input
+qtot = 0.0
+constraints = np.genfromtxt('constraints/'+AA+'idx.csv', delimiter=';')
+q0 = get_q0()
+res = scipy.optimize.minimize(cost, q0, method='SLSQP')
+print(res)
+
+s.write_xyz('mol.xyz')
+s.write_grid('grid.xyz')
