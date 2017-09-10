83d54c0ef60364b287a7f640d16b38d44bfe324a
Melisius <erikkjellgren@live.dk>
2017-09-10 18:47:13 +0200

    Made script to write parameterfile

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import numpy as np
 
+
 def test_constraints():
+    # Test that atoms constrained to same charge are the same element
     AA = ['ALA', 'ARG', 'ASH', 'ASN', 'ASP',  'CYD', 'CYS', 'CYX', 'GLH', 'GLN', 'GLU', 'GLY', 'HID', 'HIE', 'HIS', 'ILE', 'LEU', 'LYD', 'LYS', 'MET', 'PHE', 'PRO', 'SER', 'THR', 'TRP', 'TYR', 'VAL']
     for i in range(0, len(AA)):
         AAloaded = np.genfromtxt('constraints/'+str(AA[i])+'idx.csv', delimiter=';', dtype='str')
         for j in range(0, len(AAloaded)):
             assert AAloaded[int(AAloaded[i,2])-1,0] == AAloaded[i,0]
 
+
 def test_constraints_caps():
+    # Test that atoms constrained to same charge are the same element
     AA = ['ALA', 'ARG', 'ASH', 'ASN', 'ASP',  'CYD', 'CYS', 'CYX', 'GLH', 'GLN', 'GLU', 'GLY', 'HID', 'HIE', 'HIS', 'ILE', 'LEU', 'LYD', 'LYS', 'MET', 'PHE', 'PRO', 'SER', 'THR', 'TRP', 'TYR', 'VAL']
     captype = ['methylcharged','methylneutral','chargedmethyl','neutralmethyl']
     for i in range(0, len(AA)):
@@ -15,3 +19,26 @@ def test_constraints_caps():
             AAloaded = np.genfromtxt('constraints/'+str(AA[i])+str(captype[j])+'idx.csv', delimiter=';', dtype='str')
             for k in range(0, len(AAloaded)):
                 assert AAloaded[int(AAloaded[k,2])-1,0] == AAloaded[k,0]
+
+
+def test_parameterfile():
+    # Test that atoms given the same atom type have identical parameters
+    par = np.genfromtxt('FFparameters.csv', delimiter=';', dtype=str)
+    checkdict = {}
+    for i in range(1, len(par)):
+        if par[i,0]+par[i,1]+'q' not in checkdict:
+            checkdict[par[i,0]+par[i,1]+'q']   = float(par[i,2])
+            checkdict[par[i,0]+par[i,1]+'axx'] = float(par[i,3])
+            checkdict[par[i,0]+par[i,1]+'axy'] = float(par[i,4])
+            checkdict[par[i,0]+par[i,1]+'axz'] = float(par[i,5])
+            checkdict[par[i,0]+par[i,1]+'ayy'] = float(par[i,6])
+            checkdict[par[i,0]+par[i,1]+'ayz'] = float(par[i,7])
+            checkdict[par[i,0]+par[i,1]+'azz'] = float(par[i,8])
+        else:
+            assert checkdict[par[i,0]+par[i,1]+'q']   == float(par[i,2])
+            assert checkdict[par[i,0]+par[i,1]+'axx'] == float(par[i,3])
+            assert checkdict[par[i,0]+par[i,1]+'axy'] == float(par[i,4])
+            assert checkdict[par[i,0]+par[i,1]+'axz'] == float(par[i,5])
+            assert checkdict[par[i,0]+par[i,1]+'ayy'] == float(par[i,6])
+            assert checkdict[par[i,0]+par[i,1]+'ayz'] == float(par[i,7])
+            assert checkdict[par[i,0]+par[i,1]+'azz'] == float(par[i,8])
---
+++ writeFFparameters.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+def makeparameterfiler():
+    # REMEMBER TO MAKE FIX FOR CCYX and cCYX
+    
+    parameters = np.genfromtxt('FFparameterstemplate.csv',delimiter=';',dtype=str)
+    current = 'None'
+    for i in range(1,len(parameters)):
+        
+        if current != parameters[i,0]:
+            if parameters[i,0][0] == 'C' and parameters[i,0][1] != 'Y':
+                with open('fittedparameters/'+parameters[i,0][1:]+'chargedmethyl_q_out.txt') as f:
+                    fitoutput = list(f)
+                for j in range(0, len(fitoutput)):
+                    if fitoutput[j][0:3] == 'nit':
+                        charges = fitoutput[j+2+6:len(fitoutput)]
+                        for k in range(0, len(charges)):
+                            parameters[i+k,2] = charges[k]
+                            
+            elif parameters[i,0][0] == 'c':
+                with open('fittedparameters/'+parameters[i,0][1:]+'neutralmethyl_q_out.txt') as f:
+                    fitoutput = list(f)
+                for j in range(0, len(fitoutput)):
+                    if fitoutput[j][0:3] == 'nit':
+                        charges = fitoutput[j+2+6:len(fitoutput)]
+                        for k in range(0, len(charges)):
+                            parameters[i+k,2] = charges[k]
+                            
+            elif parameters[i,0][0] == 'N':
+                with open('fittedparameters/'+parameters[i,0][1:]+'methylcharged_q_out.txt') as f:
+                    fitoutput = list(f)
+                for j in range(0, len(fitoutput)):
+                    if fitoutput[j][0:3] == 'nit':
+                        charges = fitoutput[j+2:len(fitoutput)-6]
+                        for k in range(0, len(charges)):
+                            parameters[i+k,2] = charges[k]
+                
+            elif parameters[i,0][0] == 'n':
+                with open('fittedparameters/'+parameters[i,0][1:]+'methylneutral_q_out.txt') as f:
+                    fitoutput = list(f)
+                for j in range(0, len(fitoutput)):
+                    if fitoutput[j][0:3] == 'nit':
+                        charges = fitoutput[j+2:len(fitoutput)-6]
+                        for k in range(0, len(charges)):
+                            parameters[i+k,2] = charges[k]
+                
+            else:
+                with open('fittedparameters/'+parameters[i,0]+'_q_out.txt') as f:
+                    fitoutput = list(f)
+                for j in range(0, len(fitoutput)):
+                    if fitoutput[j][0:3] == 'nit':
+                        charges = fitoutput[j+2+6:len(fitoutput)-6]
+                        for k in range(0, len(charges)):
+                            parameters[i+k,2] = charges[k]
+                    
+        current = parameters[i,0]
+    
+    FFpar = open('FFparameters.csv', 'w')
+    for i in range(0, len(parameters)):
+        FFpar.write(parameters[i,0])
+        FFpar.write(', ')
+        FFpar.write(parameters[i,1])
+        FFpar.write(', ')
+        FFpar.write(parameters[i,2])
+        FFpar.write(', ')
+        FFpar.write(parameters[i,3])
+        FFpar.write(', ')
+        FFpar.write(parameters[i,4])
+        FFpar.write(', ')
+        FFpar.write(parameters[i,5])
+        FFpar.write(', ')
+        FFpar.write(parameters[i,6])
+        FFpar.write(', ')
+        FFpar.write(parameters[i,7])
+        FFpar.write(', ')
+        FFpar.write(parameters[i,8])
+        FFpar.write('\n')
+    FFpar.close()
+
+makeparameterfiler()
